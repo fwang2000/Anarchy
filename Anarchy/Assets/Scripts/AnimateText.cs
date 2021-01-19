841de0bce65dfc773c358d0a26f20691454aee53
fwang2000 <61210395+fwang2000@users.noreply.github.com>
2021-01-19 15:18:22 -0800

    First Commit

---
+++ Anarchy/Assets/Scripts/AnimateText.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AnimateText : MonoBehaviour
+{
+    private int maxFontSize = 20;
+    private float fontScale = 1f;
+    private float spaceScale = 1f;
+
+    private TextMeshProUGUI text;
+
+    public void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+    }
+
+    public void Animate(float scaleTime)
+    {
+        if (text.fontSize < maxFontSize)
+        {
+            text.fontSize += fontScale / scaleTime * Time.deltaTime;
+            text.characterSpacing += spaceScale / scaleTime * Time.deltaTime;
+        }
+    }
+
+    public void Reset(float scaleTime)
+    {
+        if (text.fontSize > 16)
+        {
+            text.fontSize -= fontScale / scaleTime * Time.deltaTime;
+            text.characterSpacing -= spaceScale / scaleTime * Time.deltaTime;
+        }
+    }
+}
